14252ada61e300abb710ea848144ec763b00c973
Glenn Lefevere <glennlefevere@hotmail.com>
2019-08-10 20:43:04 +0200

    fixed some minor issues

--- src/state/files/default/statemanagement/state/__name@dasherize__-root.state.ts
+++ src/state/files/default/statemanagement/state/__name@dasherize__-root.state.ts
@@ -1,5 +1,6 @@
-<% if(data){ %>import {<%= classify(name) %>DataState} from './data/<%= dasherize(name) %>-data.state';<% } %><% if (container) {%>import {<%= classify(name) %>ContainerState} from './container/<%= dasherize(name) %>-container.state';<% } %>
+<% if(data){ %>import {<%= classify(name) %>DataState} from './data/<%= dasherize(name) %>-data.state';<% } %>
+<% if (container) {%>import {<%= classify(name) %>ContainerState} from './container/<%= dasherize(name) %>-container.state';<% } %>
 export interface <%= classify(name) %>RootState {<% if (container) {%>
-  container: <%= classify(name) %>ContainerState<%} if (container && data){  %>,<% } %><% if(data){ %>
-  data: <%= classify(name) %>DataState<% } %>
+  container: <%= classify(name) %>ContainerState;<% if(data){ %>
+  data: <%= classify(name) %>DataState;<% } %>
 }
