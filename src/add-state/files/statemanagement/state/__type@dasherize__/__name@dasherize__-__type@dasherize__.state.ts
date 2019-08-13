d8b4c4e6a6bf8e4d70c5fe83156494a30967018b
Glenn Lefevere <glennlefevere@hotmail.com>
2019-08-13 19:00:49 +0200

    fixed typo

--- src/add-state/files/statemanagement/state/__type@dasherize__/__name@dasherize__-__type@dasherize__.state.ts
+++ src/add-state/files/statemanagement/state/__type@dasherize__/__name@dasherize__-__type@dasherize__.state.ts
@@ -1,5 +1,5 @@
 export type <%= concatAndClassify(name, type) %>State = Readonly<{
 }>;
 
-export const intitial<%= concatAndClassify(name, type) %>State: <%= concatAndClassify(name, type) %>State = {
+export const initial<%= concatAndClassify(name, type) %>State: <%= concatAndClassify(name, type) %>State = {
 };
