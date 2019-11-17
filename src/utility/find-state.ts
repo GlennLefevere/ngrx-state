295a59b689d9f6e26cf03723bf901a0fd092c0d0
Glenn Lefevere <glennlefevere@hotmail.com>
2019-11-17 15:07:30 +0100

    fixed minor issues

--- src/utility/find-state.ts
+++ src/utility/find-state.ts
@@ -126,7 +126,7 @@ export function buildAddStateLevelChangesContext(context: AddStateLevelChangesCo
 }
 
 function createStateLevelTypeChanges(nodes: ts.Node[], options: any, context: AddStateLevelChangesContext): Change {
-    const toAdd = '\n  ' + options.name + (options.array ? 's' : '') + ': ' + context.className + (options.array ? '[]' : '');
+    const toAdd = '\n  ' + options.name + (options.array ? 's' : '') + ': ' + context.className + (options.array ? '[],' : ',');
     const stateTypeDef = nodes.find(n => n.kind === ts.SyntaxKind.TypeAliasDeclaration);
 
     if (!stateTypeDef) {
@@ -141,7 +141,7 @@ function createStateLevelTypeChanges(nodes: ts.Node[], options: any, context: Ad
 }
 
 function createInitialStateLevelChanges(nodes: ts.Node[], options: any, context: AddStateLevelChangesContext): Change {
-    const toAdd = '\n  ' + options.name + (options.array ? 's' : '') + ': ' + (options.array ? '[]' : 'undefined');
+    const toAdd = '\n  ' + options.name + (options.array ? 's' : '') + ': ' + (options.array ? '[],' : 'undefined,');
 
     const stateVariableDeclaration = nodes.find(n => n.kind === ts.SyntaxKind.VariableDeclaration);
 
